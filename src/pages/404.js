6b53495dd50998265b1f9677c94f82516a31e694
Huy Taing <Taing.huy@outlook.fr>
2019-11-21 00:24:14 +0100

    modification de la page 4O4.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,9 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <div className="container error">
+      <h1>Désolé, cette page n'est pas disponible</h1>
+    </div>
   </Layout>
 )
 
